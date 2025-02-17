7828fcd73685e4f866c30e47943f17de054cc477
savchukeugene <savchukeugene.work@gmail.com>
2025-02-17 19:22:07 +0300

    Big changes

--- frontend/src/components/LandingPages/index.tsx
+++ frontend/src/components/LandingPages/index.tsx
@@ -3,7 +3,7 @@ import React, {FC} from "react";
 const LandingPages: FC = (): React.JSX.Element => {
     return (
         <>
-            Редактирование
+            Базевич Давид
         </>
     )
 }
