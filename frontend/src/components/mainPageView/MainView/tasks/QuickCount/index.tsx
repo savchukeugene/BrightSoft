142e77fefb268bec4b6f7d2984f8ccd85d937492
Matthew Markovets <mmarkovec15072003@gmail.com>
2025-02-20 13:15:42 +0300

    Simple landing

--- frontend/src/components/mainPageView/MainView/tasks/QuickCount/index.tsx
+++ frontend/src/components/mainPageView/MainView/tasks/QuickCount/index.tsx
@@ -1,11 +1,63 @@
-import {FC} from "react";
+import { FC } from 'react';
+import { Button, Form, Input } from 'antd';
+import FormItem from 'antd/es/form/FormItem';
+import { messages } from '../../../../../common/constants/messages.ts';
+
+import s from './styles.module.scss';
 
 const QuickCount: FC = (): JSX.Element => {
-    return (
-        <>
-            Quick Count
-        </>
-    )
-}
+  return (
+    <div>
+      <h1>{messages.view.main.tasks.quickCount.title}</h1>
+      <section>
+        <h1> {messages.view.main.tasks.quickCount.formTitle}</h1>
+        <Form className={s.quickCountForm}>
+          <FormItem
+            name={'duration'}
+            label={messages.view.main.tasks.quickCount.duration}
+            rules={[
+              {
+                required: true,
+                message:
+                  messages.view.main.tasks.quickCount.validateMessage.validateDuration,
+              },
+            ]}
+          >
+            <Input />
+          </FormItem>
+          <FormItem
+            name={'period'}
+            label={messages.view.main.tasks.quickCount.period}
+            rules={[
+              {
+                required: true,
+                message:
+                  messages.view.main.tasks.quickCount.validateMessage.validatePeriod,
+              },
+            ]}
+          >
+            <Input />
+          </FormItem>
+          <FormItem
+            name={'range'}
+            label={messages.view.main.tasks.quickCount.range}
+            rules={[
+              {
+                required: true,
+                message:
+                  messages.view.main.tasks.quickCount.validateMessage.validateRange,
+              },
+            ]}
+          >
+            <Input />
+          </FormItem>
+          <FormItem>
+            <Button htmlType={'submit'}>{messages.button.continue}</Button>
+          </FormItem>
+        </Form>
+      </section>
+    </div>
+  );
+};
 
-export default QuickCount
+export default QuickCount;
