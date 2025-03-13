ebaa25dc39f1a93c5ba0dcae91f2293ddd6dda99
Matthew Markovets <mmarkovec15072003@gmail.com>
2025-03-13 14:01:36 +0300

    Nest conf

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { IS_DEV_ENV } from './libs/common/utils/is-dev.util';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -8,6 +10,8 @@ import { IS_DEV_ENV } from './libs/common/utils/is-dev.util';
       ignoreEnvFile: !IS_DEV_ENV,
       isGlobal: true,
     }),
+    PrismaModule,
+    UserModule,
   ],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
+import IORedis from 'ioredis';
+
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+
+import * as session from 'express-session';
+import { ms, StringValue } from './libs/common/utils/ms.util';
+import { parseBoolean } from './libs/common/utils/parse-boolean.util';
+import { RedisStore } from 'connect-redis';
 
 dotenv.config();
 
@@ -12,6 +19,26 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
 
+  const redis = new IORedis(config.getOrThrow('REDIS_URL'));
+  app.use(
+    session({
+      secret: config.getOrThrow<string>('SESSION_SECRET'),
+      name: config.getOrThrow<string>('SESSION_NAME'),
+      resave: true,
+      saveUninitialized: false,
+      cookie: {
+        domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+        maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
+        httpOnly: parseBoolean(config.getOrThrow<string>('SESSION_HTTP_ONLY')),
+        secure: parseBoolean(config.getOrThrow<string>('SESSION_SECURE')),
+        sameSite: 'lax',
+      },
+      store: new RedisStore({
+        client: redis,
+        prefix: config.getOrThrow<string>('SESSION_FOLDER'),
+      }),
+    }),
+  );
   // Cookie adjusting
   app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
